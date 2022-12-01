0160667f7688c6437396456ceea62e50c14c3e22
nooralyasiri <noor.alyasiri@mavs.uta.edu>
2022-12-01 15:05:17 -0600

    style changes
    - basic ui html/css
    - script.js added

---
+++ script.js
@@ -0,0 +1,27 @@
+// this is how you fetch info from an api and include it in your browser extension/site 
+async function fetchData() {
+  const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
+  const record=await res.json();
+  document.getElementById("date").innerHTML=record.data[0].date;
+  document.getElementById("areaName").innerHTML=record.data[0].areaName;
+  document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
+  document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+}
+fetchData(); 
+
+function fetchCatImage(){
+  let image=document.getElementById("cat-image")
+  fetch('https://api.thecatapi.com/v1/images/search')
+  .then(res => res.json())
+  .then(.json => image.src = json[0].url)
+}
+
+function btnClick(){
+  let button = document.getElementById("new-cat-btn")
+  button.addEventListener("click", fetchCatImage)
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchCatImage()
+  btnClick()
+})
